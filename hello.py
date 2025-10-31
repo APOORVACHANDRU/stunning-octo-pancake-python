e87e2615d6df4f91314b5f20752b5264ef9c07ff
Apoorva Chandrashekar <apoorva.rsp@gmail.com>
2025-10-31 12:29:34 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("My first program")
+price = 100
+qty = 5
+total = price * qty
+print("Total FIRST COMMIT =", total)
+
+if True:
+   print ("True")
+   print ("Answer")
+else:
+   print ("False")
