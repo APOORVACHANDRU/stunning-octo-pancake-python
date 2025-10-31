d108b2ded56a57c4a8ec59839ecc335c846dcf5f
Apoorva Chandrashekar <apoorva.rsp@gmail.com>
2025-10-31 19:36:59 +0100

    variables and strings

--- hello.py
+++ hello.py
@@ -1,11 +1,50 @@
-print("My first program")
-price = 100
-qty = 5
+print("My first's program",end="")
+price,qty = 100,10
+rating = 2.5
 total = price * qty
-print("Total FIRST COMMIT =", total)
+print(5+3)
+print(5*3)
+print("Total \"FIRST\" 'COMMIT' =", total, rating)
+x = "Python"
+y = "is"
+z = "awesome"
+print(price ,y)
 
 if True:
-   print ("True")
-   print ("Answer")
+     print (10*"True")
+     print ("Ans\nwer")
 else:
-   print ("False")
+     print ('False')
+
+print("*"*3)
+
+x: int=5
+x="appu"
+print(x)
+
+print(type(x))
+
+#camelCase
+#PascalCase
+#snake_case
+
+def myfunc():
+   x = "fanstastic"
+   print("inside",x)
+
+myfunc()
+
+print("outside",x)
+
+
+b = "hello hello how r u hello who are you hello"
+
+print(b.replace("hello", "hi",2))
+
+# f-strings ( to combine string and integer )
+a = 56
+price = 59
+txt = f"my age is {a}"
+txt1 = f"The price is {price:.2f} dollars"
+txt2 = f"the multiplied value is {25*5}"
+print(txt2)
